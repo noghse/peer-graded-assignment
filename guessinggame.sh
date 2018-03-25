159e85d2332183882d1090d02cd884cae367a376
noghse <rezanoghabaie@hotmail.com>
2018-03-25 11:25:09 -0500

    updated guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,22 +4,22 @@
 
 while :
 do
-   echo "Enter a guess"
+   echo "Guess a number of files in the current directory!"
    read guess
 
    if (( guess == 3 ))
    then
-	echo "Great guess congratulations!"
+	echo "Great your guess was correct congratulations!"
 	exit
 elif [[ $guess -lt 3 ]]
 
 then
-	echo "Too bad number is too low try again!"
+	echo "Too bad your guess is too low try again!"
 
 elif [[ $guess -gt 3 ]]
 
 then
-	echo "Tood bad number is too high try again!"
+	echo "Tood bad your guess is too high try again!"
 
    fi
 
